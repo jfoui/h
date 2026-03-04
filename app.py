71ff41c12deaf613742f950e750d45ba7b8efec8
jfoui <159879360+jfoui@users.noreply.github.com>
2026-03-05 00:25:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ import pandas as pd
 
 # Load preprocess and model from MLflow
 # Load preprocessor
-scaler = joblib.load("artifacts/preprocessor.pkl")
-model = joblib.load("artifacts/model.pkl")
+scaler = joblib.load("preprocessor.pkl")
+model = joblib.load("model.pkl")
 
 def main():
     st.title('Machine Learning Heart Attack Prediction Model Deployment')
@@ -42,5 +42,6 @@ def make_prediction(features):
 
 if __name__ == '__main__':
     main()
+
 
 
